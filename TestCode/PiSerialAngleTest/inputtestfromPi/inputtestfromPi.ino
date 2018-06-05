b4d158f47969ce807fe29d100cdc6eb045e115e7
Shane Eastwood <sjeastwood72@gmail.com>
2018-06-04 17:53:44 -0700

    implementing functions on arduino test code

---
+++ TestCode/PiSerialAngleTest/inputtestfromPi/inputtestfromPi.ino
@@ -0,0 +1,123 @@
+
+const byte maxSerial = 24;
+char fromPi[maxSerial];
+
+int motor[6];
+
+int index[5];
+
+
+boolean finished = false;
+
+void setup(){
+  Serial.begin(9600);
+
+}
+
+void loop(){
+
+  Serial.println("Ready!");
+
+  readSerial();
+  parseSerial();
+
+
+    Serial.print("First motor angle: ");
+    Serial.print(motor[0]);
+    Serial.print("\n");
+
+    Serial.print("Second motor angle: ");
+    Serial.print(motor[1]);
+    Serial.print("\n");
+
+    Serial.print("Third motor angle: ");
+    Serial.print(motor[2]);
+    Serial.print("\n");
+
+    Serial.print("Fourth motor angle: ");
+    Serial.print(motor[3]);
+    Serial.print("\n");
+
+    Serial.print("Fifth motor angle: ");
+    Serial.print(motor[4]);
+    Serial.print("\n");
+
+    Serial.print("Sixth motor angle: ");
+    Serial.print(motor[5]);
+    Serial.print("\n");
+
+  //delay(1000);
+}
+
+void readSerial(){
+
+          static byte readind = 0;
+          char currentRead;
+          char ending = ';';
+          char separator = ',';
+         
+          
+            while (Serial.available() > 0 && finished == false){
+            currentRead = Serial.read();
+        
+            if (currentRead != ending) {
+        
+              fromPi[readind] = currentRead;
+              readind++;
+        
+              if (readind >= maxSerial) {
+                readind = maxSerial - 1;
+              } 
+        
+            }
+        
+            else {
+              fromPi[readind] = currentRead;
+              fromPi[readind] = '\0';
+              //change the previous lines to store the final index instead for string parsing
+              //and motor co-ordination
+              readind = 0;
+              finished = true;
+        
+            }
+        
+          }
+  
+  }
+
+
+
+  void parseSerial() {
+
+  
+        if (finished) {
+          Serial.print("Transfer complete!\n");
+          Serial.print("The received angles are: ");
+          Serial.println(fromPi);
+      
+          int j = 0;
+          int ip = 0;
+          char *p = strtok(fromPi,",");
+      
+          while (p) {
+      
+            if(ip < 6){
+      
+              motor[ip++] = atoi(p);
+            
+            }
+      
+            p = strtok(NULL,",");
+          
+          }
+      
+          //enter the motor parsing results here
+          
+        }
+      
+        finished = false;
+  
+  
+  
+  }
+
