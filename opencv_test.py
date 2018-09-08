ae97fb65b86fc45eebcaf7503603525284423221
Shane Eastwood <sjeastwood72@gmail.com>
2018-09-08 13:51:40 -0700

    open cv and pi camera test

--- opencv_test.py
+++ opencv_test.py
@@ -1,18 +1,19 @@
 import cv2
+import numpy as np
 
 #Load an image from file
 image = cv2.imread("face.jpg", 1)
 
 #Load a cascade file for detecting faces
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.4.3/data/haarcascades/haarcascade_frontalface_alt.xml')
 
 #Convert to grayscale
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
 #Look for faces in the image using the loaded cascade file
-faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-print "Found "+str(len(faces))+" face(s)"
+print ("Found "+str(len(faces))+" face(s)")
 
 #Draw a rectangle around every found face
 for (x,y,w,h) in faces:
